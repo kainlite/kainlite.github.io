2a0310e6146d509122c4fb4a671c5e0bdc18ea06
kainlite <kainlite@gmail.com>
2014-05-06 13:23:49 -0300

    Remodel content controller

--- js/controllers/content_controller.js
+++ js/controllers/content_controller.js
@@ -1,10 +1,30 @@
-Resume.controller('contentController', function($scope, contentFactory) {
-  $scope.me = 'Pepe Pepe'
-  $scope.contact = 'pepe@gmail.com'
-  $scope.about = 'About me';
-  $scope.head = 'Résumé';
-  $scope.presentation = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+Resume.controller('contentController', ['$scope', 'contentFactory',
+  function($scope, contentFactory) {
+    $scope.me = 'Pepe Pepe'
+    $scope.contact = 'pepe@gmail.com'
+    $scope.about = 'About me';
+    $scope.head = 'Résumé';
+    $scope.presentation = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-  $scope.work_frames = contentFactory.getWorks();
-  $scope.study_frames = contentFactory.getStudies();
-});
+    getWorks();
+    getStudies();
+
+    function getWorks() {
+      contentFactory.getWorks()
+        .success(function(works) {
+          $scope.works = works;
+        })
+        .error(function(error){
+          $scope.works_status = 'Error retrieving works info! ' + error.message;
+        });
+    }
+    function getStudies() {
+      contentFactory.getStudies()
+        .success(function(studies) {
+          $scope.studies = studies;
+        })
+        .error(function(error){
+          $scope.studies_status = 'Error retrieving studies info! ' + error.message;
+        });
+    }
+}]);

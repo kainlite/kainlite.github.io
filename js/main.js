cced1f14fcb82efc65940e546702ad383d3bc5ff
kainlite <kainlite@gmail.com>
2024-03-18 01:12:16 -0300

    Last commit

--- js/main.js
+++ js/main.js
@@ -2,18 +2,23 @@
  * Created by fabiomadeira on 25/02/15.
  */
 // jQuery for page scrolling feature
-jQuery(document).ready(function(e) {
-    e(".scroll").click(function(t) {
-        t.preventDefault();
-        e("html,body").animate({
-            scrollTop: e(this.hash).offset().top
-        }, 1e3)
-    });
+jQuery(document).ready(function (e) {
+  e(".scroll").click(function (t) {
+    t.preventDefault();
+    e("html,body").animate(
+      {
+        scrollTop: e(this.hash).offset().top,
+      },
+      1e3
+    );
+  });
 
-    $(function() {
-    	$('img').on('click', function() {
-			$('.enlargeImageModalSource').attr('src', $(this).attr('src'));
-			$('#enlargeImageModal').modal('show');
-		});
-	});
+  $(function () {
+    $("img").on("click", function () {
+      $(".enlargeImageModalSource").attr("src", $(this).attr("src"));
+      $("#enlargeImageModal").modal("show");
+    });
+  });
+
+  document.getElementById("consent-overlay").classList.toggle("active");
 });
